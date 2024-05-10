baf431303a350aac6f678653ecf6ccbe5b74b717
Zelalem Tefera <zedoyetefe@gmail.com>
2024-05-10 18:48:35 +0300

    commit with full functional js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 
-const display1 = document.getElementById("display1");
-const display2 = document.getElementById("display2");
+const Display1 = document.getElementById("display1");
+const Display2 = document.getElementById("display2");
 
-let counter = 1;
+let x_Vs_o = 1;
 let p1Counter = 0;
 let p2Counter = 0;
+let preId="";
 
 function changeButton(id) {
     const button = document.getElementById(id);
-    if (counter % 2 === 1) {
-        button.innerHTML = 'X';
-    } else {
-        button.innerHTML = 'O';
+    if(id!==preId){
+        if (x_Vs_o % 2 === 1) {
+            button.innerHTML = 'X';
+            preId=id;
+        } else {
+            button.innerHTML = 'O';
+            preId=id;
+        }
+        x_Vs_o++;
+        checkWinner();
     }
-    counter++;
-    checkWinner();
 }
 
 function checkWinner() {
+   
     const b1 = document.getElementById('b1').innerHTML;
     const b2 = document.getElementById('b2').innerHTML;
     const b3 = document.getElementById('b3').innerHTML;
@@ -36,9 +42,10 @@ function checkWinner() {
         (b3 === "X" && b6 === "X" && b9 === "X") || 
         (b1 === "X" && b5 === "X" && b9 === "X") || 
         (b3 === "X" && b5 === "X" && b7 === "X")) {
-            console.log("condition1 cheked");
         p1Counter++;
-        display1.innerHTML = "frgrthtr";
+        x_Vs_o=1;
+        display1.value= `${p1Counter} win`;
+        window.setTimeout(reset,1500);
         return;
     }
     
@@ -50,9 +57,10 @@ function checkWinner() {
         (b3 === "O" && b6 === "O" && b9 === "O") || 
         (b1 === "O" && b5 === "O" && b9 === "O") || 
         (b3 === "O" && b5 === "O" && b7 === "O")) {
-            console.log("condition1 cheked");
         p2Counter++;
-        display2.innerHTML= p2Counter;
+        x_Vs_o=0;
+        display2.value = `${p2Counter} win`;
+        window.setTimeout(reset,1500);
         return;  
         
     }
@@ -69,6 +77,6 @@ function checkWinner() {
         document.getElementById('b7').innerHTML="";
         document.getElementById('b8').innerHTML="";
         document.getElementById('b9').innerHTML="";
-        counter=1;
+        preId="";
         }
     
